7c9d34e92fb5980a9e08613f42bc047c39875602
SaintAM <mistersteepstar@gmail.com>
2024-05-09 22:00:22 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "productlist",
+    element: <ProductList />,
+  },
+  {
+    path: "form",
+    element: <Form />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
