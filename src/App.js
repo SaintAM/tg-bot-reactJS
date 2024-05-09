34218488e1429f4d78f878130e6252790ebe9be9
SaintAM <mistersteepstar@gmail.com>
2024-05-09 15:24:45 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import { useEffect } from "react";
 import "./App.css";
-const tg = window.Telegram.WebApp;
+
 
 function App() {
-  const onClose = () => {
-    tg.close();
-  };
+
 
   useEffect(() => {
     tg.ready();
   }, []);
-  
+
   return (
     <div className="app">
       front channed
-      <button onClick={onClose}>Закрыть</button>
+      
     </div>
   );
 }
