e45d5905a515cdd4409bb820fcce4d8589431dfa
SaintAM <mistersteepstar@gmail.com>
2024-05-09 16:10:49 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import { useTelegram } from "./hooks/useTelegram";
+const tg = window.Telegram.WebApp;
 
 function App() {
-  const { tg, onToggleButton } = useTelegram();
+  const { onToggleButton } = useTelegram();
 
   useEffect(() => {
     tg.ready();
